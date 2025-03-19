ebc202e843048ad19149d61c99c8b346b66ac892
Dale Ward <pogonomyrmex@gmail.com>
2025-03-19 09:05:47 -0600

    starting to work on frontmatter processing

--- format-post.py
+++ format-post.py
@@ -1,15 +1,33 @@
+import os
+import sys
+from argparse import ArgumentParser
 
-# Python program to read 
-# file word by word
- 
-# opening the text file
-with open('README.md','r') as file:
- 
+in_filename = 'index.md'
+out_filename = 'test.md'
+print(sys.argv[1:])
+path = sys.argv[1];
+in_file_name = os.path.join(path, in_filename)
+out_file_name = os.path.join(path, out_filename )
+in_file = open(in_file_name, "r")
+out_file = open(out_file_name, "w")
+handled_front_matter = False
+handling_front_matter = False
+
     # reading each line    
-    for line in file:
- 
-        # reading each word        
-        for word in line.split():
- 
-            # displaying the words           
-            print(word) 
+for line in in_file:
+    if (handled_front_matter == False and line == '---'):
+        handling_front_matter = True
+        out_file.write(line)
+    else:
+        if (handling_front_matter):
+            if (line == '---'):
+                out_file.write(line)
+                handled_front_matter = True
+                handling_front_matter = False
+
+        else:
+            out_file.write_line
+        
+    
+in_file.close()
+out_file.close()
