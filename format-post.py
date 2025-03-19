634b024cfefd0929690d558a199aae571c678f49
Dale Ward <pogonomyrmex@gmail.com>
2025-03-19 09:41:50 -0600

    front matter processing still

--- format-post.py
+++ format-post.py
@@ -12,21 +12,45 @@ in_file = open(in_file_name, "r")
 out_file = open(out_file_name, "w")
 handled_front_matter = False
 handling_front_matter = False
+postdate = "" 
 
-    # reading each line    
+    
 for line in in_file:
-    if (handled_front_matter == False and line == '---'):
+    print(line)
+    out = ""
+    if (handled_front_matter == False and not handling_front_matter and line == '---\n'):
         handling_front_matter = True
         out_file.write(line)
     else:
         if (handling_front_matter):
-            if (line == '---'):
+            if (line == '---\n'):
                 out_file.write(line)
                 handled_front_matter = True
                 handling_front_matter = False
-
+            else:
+                if (line.startswith("title:")):
+                    out = line.replace("\"", "")
+                #if (line[0] != ' '):
+                if (line.startswith("date:")):
+                    postdate = line.replace("date:", "")
+                    postdate = postdate.replace("\"", "")
+                    postdate =  postdate.strip()
+                    out = "date: " + postdate + " 02:02:02 +0700" +"\n"
+                if (line.startswith("coverImage:")):
+                    out = line.replace("coverImage", "")
+                    out = out.replace("\"", "")
+                    out = out.strip()
+                    out = "image: " + "images/" + out
+                if (line.startswith("categories")):
+                    out = line
+                if (line.startswith("tags")):
+                    out = line
+                if (line.startswith("description:")):
+                    out = line
+                out_file.write(out)
         else:
-            out_file.write_line
+
+            out_file.write(line)
         
     
 in_file.close()
