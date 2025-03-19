1f600c8f08f59f5cb4b869e172a68c6ceabb99f3
Dale Ward <pogonomyrmex@gmail.com>
2025-03-19 08:12:15 -0600

    bare bones file read

---
+++ format-post.py
@@ -0,0 +1,15 @@
+
+# Python program to read 
+# file word by word
+ 
+# opening the text file
+with open('README.md','r') as file:
+ 
+    # reading each line    
+    for line in file:
+ 
+        # reading each word        
+        for word in line.split():
+ 
+            # displaying the words           
+            print(word) 
