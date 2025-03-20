e52da344aa2428a30ac94f59e50085b8f1634655
Dale Ward <pogonomyrmex@gmail.com>
2025-03-20 09:06:11 -0600

    Reorg headmatter code, fight with outfile name and image tag.

--- format-post.py
+++ format-post.py
@@ -3,6 +3,9 @@ import sys
 import re
 
 imagePath = "images"
+postdate = "" 
+title = ""
+newfilename = ""
 
 def processImageTag(inFile):
     # if we're here, our caller has read in a "<figure>" tag. Process untill we get to the
@@ -31,9 +34,54 @@ def processImageTag(inFile):
     outText = outText + "\n"
     return outText
 
+def processFrontMatter(inFile):
+    global newfilename
+    outText = "---\n"
+    line = ""
+    currentText = ""
+    description = ""
+    while (line := inFile.readline()):
+        if (line.startswith("---\n")):
+            # if we're here - found end of front matter tag. We're done.
+            break
+        if (line.startswith("title:")):
+            outText = outText + line.replace("\"", "" )
+        if (line.startswith("date:")):
+            postdate = line.replace("date:", "")
+            postdate = postdate.replace("\"", "")
+            postdate =  postdate.strip()
+            newfilename = postdate
+            outText =  outText + "date: " + postdate + " 02:02:02 +0700" +"\n"
+        if (line.startswith("coverImage:")):
+            currentText = line.replace("coverImage:", "")
+            currentText = currentText.replace("\"", "")
+            currentText = currentText.strip()
+            outText = outText + "image: " + os.path.join(imagePath, out) + currentText + "\n"
+        if (line.startswith("categories:")):
+            outText = outText + line
+        if (line.startswith("tags:")):
+            outText = outText + line
+        if (line.startswith("title:")):
+            title = line.replace("title:", "")
+            title = title.replace("\"", "")
+            title = title.strip()
+            outText = outText + title + "\n"
+        if (line.startswith("description:")):
+            description = line
+            # description = line.replace("description:", "")
+            description = description.strip()
+            outText = outText + line + "\n"
+    if (len(description) == 0):
+        outText = outText + "description: \n"
+    newfilename = re.sub(r'[^\w\s]', '', title)
+    newfilename = "-" + newfilename.replace("'", "")
+    newfilename = postdate + newfilename + ".md"
+    #globals()[newfilename] = newfilename
+    outText = outText + "---\n"
+    return outText
+
 in_filename = 'index.md'
 out_filename = 'test.md'
-#print(sys.argv[1:])
 path = sys.argv[1]
 in_file_name = os.path.join(path, in_filename)
 out_file_name = os.path.join(path, out_filename )
@@ -41,68 +89,22 @@ in_file = open(in_file_name, "r")
 out_file = open(out_file_name, "w")
 handled_front_matter = False
 handling_front_matter = False
-postdate = "" 
-description = ""
-title = ""
-newfilename = ""
     
 for line in in_file:
     print(line)
     out = ""
-    if (handled_front_matter == False and not handling_front_matter and line == '---\n'):
-        # if we're here, we have not yet handled front matter, but have read the tag starting it
-        handling_front_matter = True
-        out_file.write(line)
+    if (line.startswith("---\n")and not handled_front_matter):
+        frontMatterText = processFrontMatter(in_file)
+        out_file.write(frontMatterText)
+        handled_front_matter = True
     else:
-        if (handling_front_matter):
-            # if we're here, we're currently processing the front matter contents.
-            if (line == '---\n'):
-                # if we're here, we're closing the end matter tag
-                if (len(description) == 0):
-                    out_file.write("description: \n")
-                out_file.write(line)
-                handled_front_matter = True
-                handling_front_matter = False
-            else:
-                if (line.startswith("title:")):
-                    out = line.replace("\"", "")
-                #if (line[0] != ' '):
-                if (line.startswith("date:")):
-                    postdate = line.replace("date:", "")
-                    postdate = postdate.replace("\"", "")
-                    postdate =  postdate.strip()
-                    newfilename = postdate
-                    out = "date: " + postdate + " 02:02:02 +0700" +"\n"
-                if (line.startswith("coverImage:")):
-                    out = line.replace("coverImage:", "")
-                    out = out.replace("\"", "")
-                    out = out.strip()
-                    out = "image: " + os.path.join(imagePath, out) + "\n"
-                if (line.startswith("categories:")):
-                    out = line
-                if (line.startswith("tags:")):
-                    out = line
-                if (line.startswith("title:")):
-                    title = line.replace("title:", "")
-                    title = title.replace("\"", "")
-                    title = title.strip()
-                if (line.startswith("description:")):
-                    description = out
-                    description = out.replace("description:", "")
-                    description = description.strip()
-                    out = line
-                out_file.write(out)
-        else:
-            # if we're here, we are not processing front matter. Likely the body of the post.
-            if (line.startswith("<figure>")):
-                imageTag = processImageTag(in_file)
-                out_file.write(imageTag)
-            else:
-                out_file.write(line)
+        if (line.startswith("<figure>")):
+            imageTag = processImageTag(in_file)
+            out_file.write(imageTag)
+        else: 
+            out_file.write(line)
         
     
 in_file.close()
 out_file.close()
-title = re.sub(r'[^\w\s]', '', title)
-newfilename = postdate + "-" + title.replace("'", "") + ".md"
 os.rename (os.path.join(path, out_filename), os.path.join(path, newfilename))
